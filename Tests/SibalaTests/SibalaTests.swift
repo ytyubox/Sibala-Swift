9d07dea584628b47e085204ba7efb9748bc401cd
Tsungyu Yu <ytyubox@gmail.com>
2021-03-06 21:16:05 +0800

    Refactor: from Sibala() to makeSUT()

--- Tests/SibalaTests/SibalaTests.swift
+++ Tests/SibalaTests/SibalaTests.swift
@@ -16,7 +16,7 @@ class Sibala {
 final class SibalaTests: XCTestCase {
 
     func test_TiewhenBothNoPoint() {
-        let sut = Sibala()
+        let sut = makeSUT()
         
         let result = sut.game("testwinner: 1 2 3 4  testloser: 1 2 3 4")
         
@@ -24,10 +24,14 @@ final class SibalaTests: XCTestCase {
     }
     
     func test_resultIsPrefixWithWinnerName() {
-        let sut = Sibala()
+        let sut = makeSUT()
         let result = sut.game("alwaysWinner: 1 1 3 4  alwaysLoser: 1 2 3 4")
         
         
         XCTAssertTrue(result.hasPrefix("alwaysWinner"), "`alwaysWinner should be at prefix of result`")
+    }
+    // MARK: - helper
+    func makeSUT() -> Sibala {
+        return Sibala()
     }
 }
