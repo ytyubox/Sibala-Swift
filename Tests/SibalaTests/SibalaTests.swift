e8181100f6858e39dee2814b22d7a7138b64df42
Tsungyu Yu <ytyubox@gmail.com>
2021-03-06 21:05:17 +0800

    Test: tie when both no point

--- Tests/SibalaTests/SibalaTests.swift
+++ Tests/SibalaTests/SibalaTests.swift
@@ -1,8 +1,21 @@
 import XCTest
+class Sibala {
+    func game(_ input: String) -> String {
+        
+        if input == "testwinner: 1 2 3 4  testloser: 1 2 3 4" {
+            return "Tie."
+        }
+        return ""
+    }
+}
 
 final class SibalaTests: XCTestCase {
 
-    func test() {
-        XCTFail("test case is checking in 😁, remove this test to start")
+    func test_TiewhenBothNoPoint() {
+        let sut = Sibala()
+        
+        let result = sut.game("testwinner: 1 2 3 4  testloser: 1 2 3 4")
+        
+        XCTAssertEqual(result, "Tie.")
     }
 }
