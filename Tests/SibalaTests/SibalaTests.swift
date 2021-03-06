bc0c40420a0be41a0a4ffe44fc5894cae077b9ef
Tsungyu Yu <ytyubox@gmail.com>
2021-03-06 20:59:27 +0800

    setup spm file

--- Tests/SibalaTests/SibalaTests.swift
+++ Tests/SibalaTests/SibalaTests.swift
@@ -1,15 +1,8 @@
 import XCTest
-@testable import Sibala
 
 final class SibalaTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Sibala().text, "Hello, World!")
-    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func test() {
+        XCTFail("test case is checking in 😁, remove this test to start")
+    }
 }
