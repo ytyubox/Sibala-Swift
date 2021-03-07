5285cb315fb451b01c9fb1aeb1087bd7839c374e
Tsungyu Yu <ytyubox@gmail.com>
2021-03-07 15:00:19 +0800

    Refactor: Extra Category init into A factory method

---
+++ Sources/Sibala/Internal/CategroyFactory.swift
@@ -0,0 +1,71 @@
+//
+/* 
+ *		Created by 游宗諭 in 2021/3/7
+ *		
+ *		Using Swift 5.0
+ *		
+ *		Running on macOS 11.2
+ */
+
+
+import Foundation
+typealias Count = Int
+private let factorys: [Count:_CategoryFactory] =
+    [
+        1: AllTheSameKindFactory(),
+        2: NoPointOrNormalFactory(),
+        3: NormalPointFactory(),
+        4: NoPointFactory(),
+    ]
+
+func CategroyFactory(dices: Player.Dices) -> Player.Category {
+    let group = Dictionary(grouping: dices.values, by: {$0}).mapValues(\.count)
+    let factory = factorys[group.count]!
+    return factory.make(group: group)
+}
+
+private protocol _CategoryFactory {
+    typealias Group = [Int: Int]
+    typealias Outpout = Player.Category
+    func make(group: Group) -> Outpout
+}
+
+
+private struct NoPointFactory:_CategoryFactory {
+    func make(group: Group) -> Outpout {
+        assert(group.count == 4)
+        return .noPoint
+    }
+}
+
+private struct NoPointOrNormalFactory: _CategoryFactory {
+    func make(group: Group) -> Outpout {
+        assert(group.count == 2)
+        if group.allSatisfy({ (k,v) -> Bool in v == 2 }) {
+            let max = group.keys.max()!
+            return .normal(point: max  * 2, dominator: max)
+        }
+        else {
+            return .noPoint
+        }
+    }
+}
+
+private struct NormalPointFactory: _CategoryFactory {
+    func make(group: Group) -> Outpout {
+        assert(group.count == 3)
+        let filterGroup =  group
+            .filter({k,v in v == 1})
+        return .normal(point: filterGroup
+                        .reduce(0) {$0 + $1.key},
+                       dominator: filterGroup.keys.max()!
+        )
+    }
+}
+
+private struct AllTheSameKindFactory: _CategoryFactory {
+    func make(group: Group) -> Outpout {
+        assert(group.count == 1)
+        return .allTheSameKind(maxValue: group.keys.first!)
+    }
+}
--- Sources/Sibala/Internal/Player.swift
+++ Sources/Sibala/Internal/Player.swift
@@ -21,7 +21,7 @@ struct Player:Comparable {
     init<S>(name: S, dices: Dices) where S: StringProtocol {
         self.name = name.description
         self.dices = dices
-        self.categroy = Category(dices: dices)
+        self.categroy = CategroyFactory(dices: dices)
     }
     public init(APlayerString input: String) {
         let splited = input.split(separator: ":")
@@ -45,81 +45,6 @@ struct Player:Comparable {
                     .compactMap(Int.init))
         }
         let values:[Int]
-    }
-    
-    enum Category{
-        private typealias Count = Int
-        private static let factorys: [Count:CategoryFactory] =
-            [
-                1: AllTheSameKindFactory(),
-                2: NoPointOrNormalFactory(),
-                3: NormalPointFactory(),
-                4: NoPointFactory(),
-                
-            ]
-        init(dices: Dices) {
-            let group = Dictionary(grouping: dices.values, by: {$0}).mapValues(\.count)
-            let factory = Self.factorys[group.count]!
-            self = factory.make(group: group)
-        }
-        
-        
-        case noPoint
-        case normal(point:Int, dominator: Int)
-        case allTheSameKind(maxValue: Int)
-        
-    }
-}
-protocol CategoryFactory {
-    typealias Group = [Int: Int]
-    typealias Outpout = Player.Category
-    func make(group: Group) -> Outpout
-}
-struct NoPointFactory:CategoryFactory {
-    func make(group: Group) -> Outpout {
-        .noPoint
-    }
-}
-struct NoPointOrNormalFactory: CategoryFactory {
-    func make(group: Group) -> Outpout {
-        assert(group.count == 2)
-        if group.allSatisfy({ (k,v) -> Bool in v == 2 }) {
-            let max = group.keys.max()!
-            return .normal(point: max  * 2, dominator: max)
-        }
-        else {
-            return .noPoint
-        }
-    }
-}
-struct AllTheSameKindFactory: CategoryFactory {
-    func make(group: Group) -> Outpout {
-        .allTheSameKind(maxValue: group.keys.first!)
-    }
-}
-struct NormalPointFactory: CategoryFactory {
-    func make(group: Group) -> Outpout {
-        let filterGroup =  group
-            .filter({k,v in v == 1})
-        return .normal(point: filterGroup
-                        .reduce(0) {$0 + $1.key},
-                       dominator: filterGroup.keys.max()!
-        )
-    }
-}
-
-extension Player.Category: Comparable { }
-
-extension Player.Category:CustomStringConvertible {
-    public var description: String {
-        switch self {
-        case .noPoint:
-            return "its a no point, not a winner"
-        case .allTheSameKind(maxValue: let maxValue):
-            return "all the same kind: \(maxValue)"
-        case .normal(point: let point, dominator: _):
-            return "normal point: \(point)"
-        }
     }
     
 }
